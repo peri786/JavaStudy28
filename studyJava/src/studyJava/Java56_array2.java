96109cf9907a078cd81d0cc226f273b86b05ccfc
presium575730 <presium575730@gmail.com>
2026-05-28 21:35:16 +0900

    56 modify

--- studyJava/src/studyJava/Java56_array2.java
+++ studyJava/src/studyJava/Java56_array2.java
@@ -13,34 +13,33 @@ public class Java56_array2 {
 //		C행 O   O   O   O   O
 //		D행 O   O   O   O   O
 //		E행 O   O   O   O   O
-		//행 입력(A-E) : B
-		//열 입력(1-5) : 3
-		//B3예약 완료!
-		
+		// 행 입력(A-E) : B
+		// 열 입력(1-5) : 3
+		// B3예약 완료!
+
 		// 좌석현황 (O=빈자리, X=예매)
-		//   1열 2열 3열 4열 5열
-		//A행 O   O   O   O   O
-		//B행 O   O   X   O   O
-		
+		// 1열 2열 3열 4열 5열
+		// A행 O O O O O
+		// B행 O O X O O
+
 		Scanner sc = new Scanner(System.in);
 		boolean[][] seats = new boolean[5][5]; // false 빈자리
-		String[] rows = {"A", "B", "C", "D", "E"};
-		
-		//예매는 3회 실행시킨다
-		
+		String[] rows = { "A", "B", "C", "D", "E" };
+
+		// 예매는 3회 실행시킨다
+
 		String[][] seatPrint = new String[5][5];
 		String rowInput = " ";
 		int colInput = 0;
 		int reserveCnt = 3;
-		
+
 		// seatPrint 일괄초기화
 		for (int i = 0; i < seatPrint.length; i++) {
 			for (int j = 0; j < seatPrint[i].length; j++) {
 				seatPrint[i][j] = "O";
 			}
 		}
-		
-		
+
 		// 좌석 출력
 		System.out.println("\t좌석현황 (O=빈자리, X=예매)");
 		System.out.println("\t1열\t2열\t3열\t4열\t5열");
@@ -51,22 +50,29 @@ public class Java56_array2 {
 			}
 			System.out.println();
 		}
-		
+
 		// 예매 3회
 		while (true) {
-			if (reserveCnt == 0) break;  // 예약 횟수가 0이 되면 종료
-			
+			if (reserveCnt == 0)
+				break; // 예약 횟수가 0이 되면 종료
+
 			int rowInt = 0; // A-E행 정수로 변환해서 저장하는 변수
-			
+
 			System.out.print("행 입력(A-E) : ");
 			rowInput = sc.next();
 			System.out.print("열 입력(1-5) : ");
 			colInput = sc.nextInt();
-			
-			rowInt = (int)(rowInput.charAt(0)) - 65;  // 각각 65를 빼서 0-4의 정수로 변환
-			
-			seats[rowInt][colInput - 1] = true;  // 입력 받은 자리 true. 1-5열이므로 colInput - 1
-			
+
+			rowInt = (int) (rowInput.charAt(0)) - 65; // 각각 65를 빼서 0-4의 정수로 변환
+
+			if (seats[rowInt][colInput - 1] == true) {
+				System.out.println("이미 예약된 자리입니다.");
+				System.out.println();
+				continue;
+			}
+
+			seats[rowInt][colInput - 1] = true; // 입력 받은 자리 true. 1-5열이므로 colInput - 1
+
 			for (int i = 0; i < seats.length; i++) {
 				for (int j = 0; j < seats[i].length; j++) {
 					if (seats[i][j] == true) {
@@ -74,23 +80,22 @@ public class Java56_array2 {
 					}
 				}
 			}
-			
+
 			System.out.println(rowInput + colInput + "예약 완료!");
 			System.out.println();
 			reserveCnt--;
 		}
-		
+
 		// 좌석 출력
-				System.out.println("\t좌석현황 (O=빈자리, X=예매)");
-				System.out.println("\t1열\t2열\t3열\t4열\t5열");
-				for (int i = 0; i < seatPrint.length; i++) {
-					System.out.print(rows[i] + "행\t");
-					for (int j = 0; j < seatPrint[i].length; j++) {
-						System.out.print(seatPrint[i][j] + "\t");
-					}
-					System.out.println();
-				}
-			
+		System.out.println("\t좌석현황 (O=빈자리, X=예매)");
+		System.out.println("\t1열\t2열\t3열\t4열\t5열");
+		for (int i = 0; i < seatPrint.length; i++) {
+			System.out.print(rows[i] + "행\t");
+			for (int j = 0; j < seatPrint[i].length; j++) {
+				System.out.print(seatPrint[i][j] + "\t");
+			}
+			System.out.println();
+		}
 
 	}
 
